7d717d0c28ebad0948b3415c7fc060c7ec6685c8
lefilou <fliousalim@gmail.com>
2018-01-04 00:15:00 +0100

    feat(login): retrieve username from authentication

--- src/main/java/org/melsif/controller/UserController.java
+++ src/main/java/org/melsif/controller/UserController.java
@@ -1,13 +1,19 @@
 package org.melsif.controller;
 
+import org.apache.log4j.Logger;
 import org.melsif.dto.UserDto;
 import org.melsif.service.UserService;
 import org.melsif.util.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private final Logger LOGGER = Logger.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
@@ -16,7 +22,10 @@ public class UserController {
     private UserMapper userMapper;
 
     @GetMapping(value = "/api/user", produces = "application/json")
-    public @ResponseBody UserDto getUserByUsername(@RequestParam(value="username") String username) {
-        return userMapper.userToUserDto(userService.getUserByUsername(username));
+    public @ResponseBody UserDto getUserByUsername(Authentication authentication) {
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            return userMapper.userToUserDto(userService.getUserByUsername(authentication.getName()));
+        } else
+            return null;
     }
 }
